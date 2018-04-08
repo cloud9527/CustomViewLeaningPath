faf2b3c146cf8cb6acb9dc43bf369d21b5c85fa6
cloud9527 <cloud09527@gmail.com>
2018-04-08 22:42:11 +0800

    1.canvas基本练习 饼形图提交

---
+++ app/src/main/java/com/example/cloud/customviewleaningpath/partOne/ViewArctwo.java
@@ -0,0 +1,37 @@
+package com.example.cloud.customviewleaningpath.partOne;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by cloud on 2018/4/5.
+ */
+
+public class ViewArctwo extends View {
+    public ViewArctwo(Context context) {
+        super(context);
+    }
+
+    public ViewArctwo(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ViewArctwo(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //
+    }
+
+    Paint mPaint = new Paint();
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // x轴正右方向是0度   按顺时针方向。
+        canvas.drawArc(100,200,500,700,90,90,true,mPaint);
+//        canvas.drawArc(100,200,500,700,30,90,true,mPaint);
+    }
+}
